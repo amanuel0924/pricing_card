b4ca2e4a6a453871e6447d2e44681f1241d0d243
amanuel tegegne <amanuel0924@gmail.com>
2023-12-26 16:09:34 +0300

    second card is completed rhe big one

--- src/App.js
+++ src/App.js
@@ -46,6 +46,48 @@ function App() {
             </button>
           </div>
         </div>
+        <div className=" rounded-md p-8 border-2 border-[#B9BBC3] text-[#ffffff] bg-[#202842] flex flex-col justify-between items-center shadow-lg w-80 h-[500px] md:h-[600px] ">
+          <div className="text-center px-8 mb-8 ">
+            <h2 className="text-2xl font-bold mb-6">Enterprise</h2>
+            <p className="flex items-center mb-8">
+              <span className="text-4xl font-[1000] tracking-tighter">
+                $2000
+              </span>
+              <span className=" self-end font-medium">/month</span>
+            </p>
+            <ul className=" space-y-3 text-md font-medium">
+              <li className="flex items-center">
+                <FaCheckCircle className=" mr-2" />
+                <span>Unlimited projects</span>
+              </li>
+              <li className="flex items-center">
+                <FaCheckCircle className=" mr-2" />
+                <span>Unlimited users</span>
+              </li>
+              <li className="flex items-center">
+                <FaCheckCircle className=" mr-2" />
+                <span>Unlimited storage</span>
+              </li>
+              <li className="flex items-center">
+                <FaCheckCircle className=" mr-2" />
+                <span>Unlimited projects</span>
+              </li>
+              <li className="flex items-center">
+                <FaCheckCircle className=" mr-2" />
+                <span>Unlimited users</span>
+              </li>
+              <li className="flex items-center">
+                <FaCheckCircle className=" mr-2" />
+                <span>Unlimited users</span>
+              </li>
+            </ul>
+          </div>
+          <div className=" w-full">
+            <button className=" rounded-md p-2 = mt-12 w-full bg-white text-[#202842] hover:scale-105 transform duration-300 ">
+              Get Started
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
