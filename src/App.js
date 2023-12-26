e4e2d824901662cf9dd2a1b1cfc998e741ec5886
amanuel tegegne <amanuel0924@gmail.com>
2023-12-26 13:51:01 +0300

    finshing container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div></div>
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col md:flex-row my-6 space-y-6 md:space-x-6 md:space-y-0 md:my-0 ">
+        <div className=" rounded-md border border-[#B9BBC3] text-[#202842]"></div>
+      </div>
+    </div>
+  )
 }
 
 export default App
